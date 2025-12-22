40538a3762664c74efaa001d7a9880b715e2c532
Chinmay <chinmayrpatil123@gmail.com>
2025-12-22 21:45:08 +0530

    minimal version done with support for GET requests

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import mimetypes
+import os
 import socket
 from typing import override
 
@@ -38,25 +40,20 @@ class HTTPServer(TCPServer):
         "Content-Type": "text/html",
     }
 
-    status_codes = {200: "OK", 404: "Not Found"}
+    # Todos to add more status codes using http.client module
+    status_codes = {200: "OK", 404: "Not Found", 501: "Not Implemented"}
 
     def handle_request(self, data):
-        # b is used because we need to send bytes and not string(due to the sockets library)
-        response_line = self.response_line(status_code=200)
+        request = HTTPRequest(data)
 
-        response_headers = self.response_headers()
+        try:
+            handler = getattr(self, "handle_%s" % request.method)
+        except AttributeError:
+            handler = self.HTTP_501_handler
 
-        # a blank line is important for the http protocol to differentiate between header and response body
-        blank_line = b"\r\n"
+        response = handler(request)
 
-        response_body = b"""<html>
-        <body>
-        <h1>Request received</h1>
-        </body>
-        </html>
-        """
-
-        return b"".join([response_line, response_headers, blank_line, response_body])
+        return response
 
     def response_line(self, status_code):
         reason = self.status_codes[status_code]
@@ -76,6 +73,64 @@ class HTTPServer(TCPServer):
             headers += "%s: %s\r\n" % (h, headers_copy[h])
 
         return headers.encode()
+
+    def handle_GET(self, request):
+        filename = request.uri.strip("/")
+
+        if os.path.exists(filename):
+            response_line = self.response_line(status_code=200)
+
+            content_type = mimetypes.guess_type(filename)[0] or "text/html"
+            extra_headers = {"Content-Type": content_type}
+
+            response_headers = self.response_headers(extra_headers)
+
+            with open(filename, "rb") as f:
+                response_body = f.read()
+
+        else:
+            response_line = self.response_line(status_code=404)
+            response_headers = self.response_headers()
+            response_body = b"<h1>404 Not Found</h1>"
+
+        blank_line = b"\r\n"
+
+        return b"".join([response_line, response_headers, blank_line, response_body])
+
+    def HTTP_501_handler(self, request):
+        response_line = self.response_line(status_code=501)
+
+        response_headers = self.response_headers()
+
+        blank_line = b"\r\n"
+
+        response_body = b"<h1>501 Not Implemented</h1>"
+
+        return b"".join([response_line, response_headers, blank_line, response_body])
+
+
+class HTTPRequest:
+    def __init__(self, data):
+        self.method = None
+        self.uri = None
+        self.http_version = "1.1"
+
+        self.parse(data)
+
+    def parse(self, data):
+        lines = data.split(b"\r\n")
+
+        request_line = lines[0]
+
+        words = request_line.split(b" ")
+
+        self.method = words[0].decode()  # convert bytes to str
+
+        if len(words) > 1:
+            self.uri = words[1].decode()
+
+        if len(words) > 2:
+            self.http_version = words[2]
 
 
 if __name__ == "__main__":
